f45eae9f1a3cd9650c9a0148eae63bb5b222f675
leo-wemyss-2024 <leo.wemyss@students.codeinstitute.net>
2026-05-12 20:23:41 +0100

    `Added resolve and optimizeDeps configurations to vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,15 @@ export default defineConfig({
     host: '127.0.0.1',
     port: 4173,
   },
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web',
+    },
+    extensions: ['.web.tsx', '.web.ts', '.web.jsx', '.web.js', '.tsx', '.ts', '.jsx', '.js', '.json'],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      resolveExtensions: ['.web.tsx', '.web.ts', '.web.jsx', '.web.js', '.tsx', '.ts', '.jsx', '.js', '.json'],
+    },
+  },
 });
